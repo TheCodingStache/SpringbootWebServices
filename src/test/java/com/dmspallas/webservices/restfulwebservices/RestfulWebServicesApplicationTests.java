993948f8556779482387f0018bd2584f43d0dcdb
TheCodingStache <cs4415161@teilar.gr>
2019-11-20 09:40:48 +0200

    Initial commit

---
+++ src/test/java/com/dmspallas/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmspallas.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
