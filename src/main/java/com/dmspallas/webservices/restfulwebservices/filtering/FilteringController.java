f8d683f932062e527b9e9dece16c21448105a90b
TheCodingStache <cs4415161@teilar.gr>
2019-12-06 16:45:54 +0200

    Dynamic filtering deployment

--- src/main/java/com/dmspallas/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/dmspallas/webservices/restfulwebservices/filtering/FilteringController.java
@@ -1,6 +1,10 @@
 package com.dmspallas.webservices.restfulwebservices.filtering;
 
 import com.dmspallas.webservices.restfulwebservices.user.UserDetails;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +14,24 @@ import java.util.List;
 @RestController
 public class FilteringController {
     @GetMapping("/filtering")
-    public UserDetails retrieveSomeBean() {
-        return new UserDetails("dmspallas", "qwerty1", "dmspallas@gmail.com");
+    public MappingJacksonValue retrieveSpecificDetails() {
+        UserDetails userDetails = new UserDetails("dmspallas", "qwerty1", "dmspallas@gmail.com");
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("username");
+        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("sensitiveData", filter);
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(userDetails);
+        mappingJacksonValue.setFilters(filterProvider);
+        return mappingJacksonValue;
     }
 
     @GetMapping("/filteringList")
-    public List<UserDetails> retrieveListOfBeans() {
-        return Arrays.asList(new UserDetails("dmspallas", "qwerty", "dmspallas@gmail.com"),
+    public MappingJacksonValue retrieveListOfBeans() {
+        List<UserDetails> detailsList = Arrays.asList(new UserDetails("dmspallas", "qwerty", "dmspallas@gmail.com"),
                 new UserDetails("generalstache", "qwertyuiop1", "thegeneralstache@gmail.com"));
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("username");
+        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("sensitiveData", filter);
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(detailsList);
+        mappingJacksonValue.setFilters(filterProvider);
+        return mappingJacksonValue;
     }
 
 }
--- src/main/java/com/dmspallas/webservices/restfulwebservices/user/UserDetails.java
+++ src/main/java/com/dmspallas/webservices/restfulwebservices/user/UserDetails.java
@@ -18,23 +18,15 @@ public class UserDetails {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+
 }
